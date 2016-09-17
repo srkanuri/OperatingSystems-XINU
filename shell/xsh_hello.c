06b5805eb2325a6a4740ebbd8ad4596636bfa595
Srikanth Kanuri <srkanuri@iu.edu>
2016-09-17 19:36:50 -0400

    Made changes to xsh.hello

--- shell/xsh_hello.c
+++ shell/xsh_hello.c
@@ -1,65 +1,41 @@
-/* xsh_date.c - xsh_date */
+/* xsh_hello.c - hello */
 
 #include <xinu.h>
 #include <string.h>
 #include <stdio.h>
 
 /*------------------------------------------------------------------------
- * xsh_date - obtain and print the current month, day, year, and time
+ * xsh_hello - Custom code implementation of hello command
  *------------------------------------------------------------------------
  */
 shellcmd xsh_hello(int nargs, char *args[]) {
 
-	int32	retval;			/* return value			*/
-	uint32	now;			/* current local time		*/
-	char	datestr[64];		/* printable date in ascii	*/
-
-	/* Output info for '--help' argument */
-
-	if (nargs == 2 && strncmp(args[1], "--help", 7) == 0) {
-		printf("Usage: %s\n\n", args[0]);
-		printf("Description:\n");
-		printf("\tGeneral command that greets you back.\n");
-		/*
-		printf("Options (one per invocation):\n");
-		printf("\t-f\tforce a time server request to be sent\n");
-		printf("\t-d\tset daylight savings time on\n");
-		printf("\t-s\tset standard time (not daylight savings)\n");
-		printf("\t-a\tset daylight savings to automatic\n");
-		printf("\t--help\tdisplay this help and exit\n");
-		*/
-		return 0;
+  int32	retval;			/* return value			*/
+  uint32	now;			/* current local time		*/
+  char	datestr[64];		/* printable date in ascii	*/
+  
+  /* Output info for '--help' argument */
+  
+  if (nargs == 2 && strncmp(args[1], "--help", 7) == 0) {
+    printf("Usage: %s\n\n", args[0]);
+    printf("Description:\n");
+    printf("\tHello command to welcome you into the world of Xinu.\n");
+    return 0;
+  }
+  
+  /* Check argument count */
+  
+  if (nargs > 2) {
+    fprintf(stderr, "%s: too many arguments\n", args[0]);
+	  fprintf(stderr, "Try '%s --help' for more information\n", args[0]);
+	  return 1;
 	}
-
-	/* Check argument count */
-
-	if (nargs > 2) {
-		fprintf(stderr, "%s: too many arguments\n", args[0]);
-		fprintf(stderr, "Try '%s --help' for more information\n",
-			args[0]);
-		return 1;
-	}
-	
-	/*
-	if (nargs == 2) {
-		if (strncmp(args[1], "-f", 3) == 0) {
-			Date.dt_bootvalid = FALSE;
-		} else if (strncmp(args[1], "-d", 3) == 0) {
-			Date.dt_daylight = DATE_DST_ON;
-		} else if (strncmp(args[1], "-s", 3) == 0) {
-			Date.dt_daylight = DATE_DST_OFF;
-		} else if (strncmp(args[1], "-a", 3) == 0) {
-			Date.dt_daylight = DATE_DST_AUTO;
-		} else {
-			fprintf(stderr, "%s: invalid argument\n", args[0]);
-			fprintf(stderr,
-				"Try '%s --help' for more information\n",
-				args[0]);
-			return 1;
-		}
-	}
-	*/
-
-	printf("Welcome to Xinu");
-	return 0;
+  
+  if (nargs == 2) {
+    printf("Hello %s, Welcome to the world of Xinu!!\n", args[1]);
+    return 0;
+  }
+  
+  printf("Welcome to Xinu\n");
+  return 0;
 }

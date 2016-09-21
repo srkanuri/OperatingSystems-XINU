f82b06f8d02b1a8744153fc7b9b016a0ac8faf7c
Srikanth Kanuri <srkanuri@iu.edu>
2016-09-21 08:46:42 -0400

    code files for A2

--- apps/consume.c
+++ apps/consume.c
@@ -1,6 +1,31 @@
 #include <xinu.h>
-//#include <prodcons.h>
+#include <prodcons.h>
+/***************************************************************
+ * consume.c - Program which consumes the resource n until count
+ *
+ * Authors: Srikanth Kanuri (srkanuri)
+ *          Mangarish Wagle (mawagle)
+ * Date Created: 09/20/2016
+ * Last Modified by: Srikanth Kanuri
+ * Date Last Modified: 09/20/2016
+ * Assignment: 2
+ ***************************************************************/
 
+//Code to consume values of global variable 'n' until the value
+//of n is less than or equal to count
+//print consumed value e.g. consumed : 8
 void consumer(int count) {
+  int cnt=0;
+  while (cnt<count){
+    if(n!=0){
+      n--;
+      printf("consumed:\t%d\n", cnt+1);
+      cnt++;
+    }
+    else{
+      sleep(10);
+      continue;
+    } 
+  }
 }
 
--- apps/produce.c
+++ apps/produce.c
@@ -1,7 +1,27 @@
 #include <xinu.h>
-//#include <prodcons.h>
+#include <prodcons.h>
+/***************************************************************
+ * produce.c - Program which produces the resource n until count
+ *
+ * Authors: Srikanth Kanuri (srkanuri)
+ *          Mangarish Wagle (mawagle)
+ * Date Created: 09/20/2016
+ * Last Modified by: Srikanth Kanuri
+ * Date Last Modified: 09/20/2016
+ * Assignment: 2
+ ***************************************************************/
 
+extern int n;
+
+//Code to produce values less than equal to count, 
+//produced value should get assigned to global variable 'n'.
+//print produced value e.g. produced : 8
 void producer(int count) {
+  int cnt=0;
+  for (cnt=0; cnt<count; cnt++){
+    n++;
+    printf("produced:\t%d\n", cnt+1);    
+  }
 }
 
 

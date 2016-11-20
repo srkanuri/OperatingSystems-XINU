a234c476e4bd4e20fe94f03c891969f1405c68c0
Mangirish Wagle <mawagle@iu.edu>
2016-11-19 21:37:04 -0500

    adding initial code

--- include/fs.h
+++ include/fs.h
@@ -70,7 +70,7 @@ int fs_write(int fd, void *buf, int nbytes);
 
 /* filesystem functions */
 int fs_mkfs(int dev, int num_inodes);
-int fs_mount(int dev);
+int fs_mount(int dev); //TODO: Implement
 
 /* filesystem internal functions */
 int fs_get_inode_by_num(int dev, int inode_number, struct inode *in);

251cf3afd5306015335c5eb2704add576251e8f1
Mangirish Wagle <mawagle@iu.edu>
2016-12-10 17:16:51 -0500

    Adding the udp_client code + gettime fix

--- system/platform/arm-bbb/gettime.c
+++ system/platform/arm-bbb/gettime.c
@@ -2,6 +2,18 @@
 
 #include <xinu.h>
 
+static uint date_time = 0;
+
+uint get_datetime(void)
+{
+    return (date_time + clktime);
+}
+
+uint set_datetime(uint dt)
+{
+    date_time = dt - clktime;
+    return (date_time + clktime);
+}
 /*------------------------------------------------------------------------
  *  gettime  -  Get xinu time in seconds past Jan 1, 1970
  *------------------------------------------------------------------------
@@ -10,18 +22,15 @@ status	gettime(
 	  uint32  *timvar		/* Location to store the result	*/
 	)
 {
-	uint32	now;			/* Current time (UCT)		*/
-	int32	retval;			/* Return value from call	*/
-
 	/* Get current time in UCT representation (GMT) */
 
-	retval = getutime(&now);
-	if (retval == SYSERR) {
-		return SYSERR;
-	}
+	//retval = getutime(&now);
+	//if (retval == SYSERR) {
+	//	return SYSERR;
+	//}
 
 	/* Adjust to xinu time and store result */
 
-	*timvar = utim2ltim(now);
+	*timvar = get_datetime();
 	return OK;
 }
